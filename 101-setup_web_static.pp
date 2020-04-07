b3347858b27fed0a9a9aac0bb9834e0b4c6fb764
Gzoref <gzoref@gmail.com>
2020-04-07 18:38:49 -0400

    Setup with Puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,13 +1,5 @@
 #Puppet execute 0
-exec {'Index':
-  command  =>'sudo apt-get -y update && sudo apt-get -y install nginx && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html" && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i "51i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;}" /etc/nginx/sites-available/default
- && sudo service nginx reload && sudo service nginx restart',
-  provider => shell,
+exec {'web_static':
+    command  => 'sudo apt-get update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/ && sudo mkdir -p /data/web_static/ && sudo mkdir -p /data/web_static/releases/ && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu: /data/ && var="location /hbnb_static {\n alias /data/web_static/current/;\n}\n" && sudo sed -i "43i${var" /etc/nginx/sites-available/default && sudo service nginx restart',
+    provider => 'shell'
 }
