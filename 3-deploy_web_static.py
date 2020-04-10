1e729fc66d34eb5a28d749dc2e33b2b5b4c01a78
Gzoref <gzoref@gmail.com>
2020-04-10 17:55:30 -0400

    Fix typo in var

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -25,28 +25,28 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-        """
-        Depploy to yoru webs server
     """
-        if exists(archive_path) is False:
-            return False
-        file_name = archive_path.split('/')[1]
-        file_path = '/data/web_static/releases'
-        try:
-            put(archive_path, '/tmp/')
-            run('mkdir -p {}{}'.format(path, file_name[:-4]))
-            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
-                                                   file_path, file_name[:-4]))
-            run('rm /tmp/{}'.format(file_name))
-            run('mv {}{}/web_static/* {}{}/'.format(file_path, file_name[:-4],
-                                                    file_path, file_name[:-4]))
-            run('rm -rf {}{}/web_static'.format(file_path, file_name[:-4]))
-            run('rm -rf /data/web_static/current')
-            run('ln -s {}{}/ /data/web_static/current'.format(file_path,
-                                                              file_name[:-4]))
-            return True
-        except:
-            return False
+    Depploy to yoru webs server
+    """
+    if exists(archive_path) is False:
+        return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases'
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}'.format(file_path, file_name[:-4]))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+                                               file_path, file_name[:-4]))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}{}/web_static/* {}{}/'.format(file_path, file_name[:-4],
+                                                file_path, file_name[:-4]))
+        run('rm -rf {}{}/web_static'.format(file_path, file_name[:-4]))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(file_path,
+                                                          file_name[:-4]))
+        return True
+    except:
+        return False
 
 
 def deploy():
