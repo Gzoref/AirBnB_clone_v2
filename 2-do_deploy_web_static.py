506917d592121af97388a924d6d689db28bd89b0
Gzoref <gzoref@gmail.com>
2020-05-29 14:27:27 -0400

    [Update] u siahd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from os.path import exists
 from datetime import datetime
 from fabric.api import local
 
-env.hosts = ['35.227.27.195', '18.215.153.232']
+env.hosts = ['35.237.103.2', '35.227.27.195']
 
 
 def do_pack():
